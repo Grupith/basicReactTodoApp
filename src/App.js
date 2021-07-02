b13c154ebc389c8cbd41af80ff47edc3e1cc1de1
Dylan Koss <dylanhkoss@gmail.com>
2021-07-02 01:57:54 -0500

    got add todo and delete todo working, worked a little on the UI too

--- src/App.js
+++ src/App.js
@@ -14,21 +14,41 @@ function App() {
       text: todo,
       completed: false,
     }
-
+    // takes the current todos state then creating a new array with a newTodo added on to the todos state
     setTodos([...todos].concat(newTodo));
     setTodo('');
+  }
 
+  const deleteTodo = (id) => {
+    const updatedTodos = [...todos].filter((todo) => todo.id !== id)
+    setTodos(updatedTodos)
+  }
+
+  const toggleCheck = (id) => {
+    const updatedTodos = [...todos].map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+    })
+   
+    setTodos(updatedTodos)
   }
 
   return (
     <div className="App">
+      <h1>Task Tracker</h1>
       <form onSubmit={handleSubmit}>
         <input type='text' onChange={(e) => setTodo(e.target.value)} value={todo} />
         <button type='submit'>AddTodo</button>
       </form>
-      {todos.map((todo) => <div>{todo.text}</div>)}
+      {todos.map((todo) => <div key={todo.id} className='todoContainer'>
+        <input type='checkbox' onChange={() => toggleCheck(todo.id)} checked={todo.completed} />
+        <div>{todo.text}</div>
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+      </div>)}
     </div>
-  );
+  )
 }
 
 export default App;
